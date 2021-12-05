292d1e1820c65a7e4e268034ab9cc04b94bfb0a9
27ugeen <27ugeen@gmail.com>
2021-12-05 21:48:40 +0200

    initial task7

---
+++ Navigation/Feed/ModelFeed.swift
@@ -0,0 +1,15 @@
+//
+//  ModelFeed.swift
+//  Navigation
+//
+//  Created by GiN Eugene on 21/11/21.
+//
+
+import Foundation
+
+final class ModelFeed {
+    
+    private let password = "psw"
+    
+    
+}
